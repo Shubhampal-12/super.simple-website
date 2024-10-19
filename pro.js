260ee5860c073cdb37cc1f37cefee31f704c8f12
Shubhampal-12 <shubhampal91324@gmail.com>
2024-10-19 11:19:30 +0530

    Add files via upload

---
+++ pro.js
@@ -0,0 +1,11 @@
+document.querySelector('.burger').addEventListener('click', () => {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+});
+
+
+ 
+
+
+
+ 
